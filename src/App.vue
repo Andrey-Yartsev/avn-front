15f083df9912bfd91a9ec28bde95b1d36948d707
Alexei Online <alexeisevua@gmail.com>
2018-09-17 22:06:50 +0300

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>
-    <link href="https://fonts.googleapis.com/css?family=Courgette|Cutive+Mono|Lato|Roboto:300,400,500,700&amp;subset=cyrillic-ext" rel="stylesheet">
-
-    <Login />
-  </div>
+	<div>
+		<header id="site_header">
+			<div class="header-wrapper">
+				<div class="container header_container">
+					<router-link class="menu-item-home active" to="/"><span>Home</span></router-link>
+					<router-link class="menu-item-explore" to="/explore"><span>Explore</span></router-link>
+					<router-link class="menu-item-notifications hidden-mobile btn-notifications" to="/notifications"><span>Notifications</span></router-link>
+					<router-link class="menu-item-notifications hidden-desktop" to="/notifications"><span>Notifications</span></router-link>
+					<router-link class="menu-item-messages showChat hidden-mobile" to="/chat"><span>Messages</span></router-link>
+					<router-link class="menu-item-messages hidden-desktop" to="/chat"><span>Messages</span></router-link>
+					<h1 class="header-logo">
+						<router-link to="/">
+							<img src="/ver_e2f80f4/design/img/logo-full.svg">
+							<img class="white-logo" src="/ver_e2f80f4/design/img/logo-full-white.svg">
+						</router-link>
+					</h1>
+				</div>
+			</div>
+		</header>
+		<router-view></router-view>
+		<link href="https://fonts.googleapis.com/css?family=Courgette|Cutive+Mono|Lato|Roboto:300,400,500,700&amp;subset=cyrillic-ext" rel="stylesheet">
+	</div>
 </template>
 
 <script>
-import Login from './components/Login.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Login
-  }
-}
+	export default {}
 </script>
 
 <style lang="scss" src="@/styles/main.scss"></style>
