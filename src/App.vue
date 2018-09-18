efb23fc123acb9898efd2ec4d67fb07ac6613349
Alexei Online <alexeisevua@gmail.com>
2018-09-18 18:08:26 +0300

    init header component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
 	<div>
-		<header id="site_header">
-			<div class="header-wrapper">
-				<div class="container header_container">
-					<nav class="header-nav hidden-mobile">
-						<router-link class="menu-item-home active" to="/"><span>Home</span></router-link>
-						<router-link class="menu-item-explore" to="/explore"><span>Explore</span></router-link>
-						<router-link class="menu-item-notifications hidden-mobile btn-notifications" to="/notifications"><span>Notifications</span></router-link>
-						<router-link class="menu-item-notifications hidden-desktop" to="/notifications"><span>Notifications</span></router-link>
-						<router-link class="menu-item-messages showChat hidden-mobile" to="/chat"><span>Messages</span></router-link>
-						<router-link class="menu-item-messages hidden-desktop" to="/chat"><span>Messages</span></router-link>
-					</nav>
-					<h1 class="header-logo">
-						<router-link to="/">
-							<img src="/ver_e2f80f4/design/img/logo-full.svg">
-							<img class="white-logo" src="/ver_e2f80f4/design/img/logo-full-white.svg">
-						</router-link>
-					</h1>
-				</div>
-			</div>
-		</header>
+		<Header></Header>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
-	export default {}
+	import Header from './components/Header';
+
+	export default {
+		components: {
+			'Header': Header,
+		}
+	}
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header id="site_header" class="">
+        <div class="header-wrapper">
+            <div class="container header_container">
+                <nav class="header-nav hidden-mobile">
+                    <router-link class="menu-item-home active" to="/"><span>Home</span></router-link>
+                    <router-link class="menu-item-explore" to="/explore"><span>Explore</span></router-link>
+                    <router-link class="menu-item-notifications hidden-mobile btn-notifications" to="/notifications"><span>Notifications</span></router-link>
+                    <router-link class="menu-item-notifications hidden-desktop" to="/notifications"><span>Notifications</span></router-link>
+                    <router-link class="menu-item-messages showChat hidden-mobile" to="/chat"><span>Messages</span></router-link>
+                    <router-link class="menu-item-messages hidden-desktop" to="/chat"><span>Messages</span></router-link>
+                </nav>
+                <h1 class="header-logo">
+                    <router-link to="/">
+                        <img src="/ver_e2f80f4/design/img/logo-full.svg">
+                        <img class="white-logo" src="/ver_e2f80f4/design/img/logo-full-white.svg">
+                    </router-link>
+                </h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
