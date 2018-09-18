75f5858142abcbf2031743f6d316fe740a440c50
Alexei Online <alexeisevua@gmail.com>
2018-09-18 20:41:55 +0300

    Update styles sources

--- src/App.vue
+++ src/App.vue
@@ -16,3 +16,8 @@
 		}
 	}
 </script>
+
+<style lang="scss" src="@/design/css/main.scss"></style>
+<style lang="scss" src="@/design/css/colorScheme.scss"></style>
+<style lang="scss" src="@/design/fonts/team/team.scss"></style>
+
