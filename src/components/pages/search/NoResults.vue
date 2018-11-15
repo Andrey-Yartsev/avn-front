37f7aab1ff3a6320029a479c0aeb9b6409de408f
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-11-15 07:54:35 +0300

    fix 'nothing found' flash on mobile if search has results

--- src/components/pages/search/NoResults.vue
+++ src/components/pages/search/NoResults.vue
@@ -4,7 +4,7 @@
       <span class="no-results-search__text">Loading</span>
     </div>
   </div>
-  <div class="no-results-search" v-else>
+  <div class="no-results-search" v-else-if="!items.length">
     <div class="no-results-search__message">
       <span class="no-results-search__text">Nothing found for&nbsp;</span>
       <span class="searchAllTag">"{{ query }}"</span>
@@ -15,7 +15,12 @@
 <script>
 export default {
   name: "SearchNoResults",
-  props: ["query", "loading"]
+  props: ["query", "loading"],
+  computed: {
+    items() {
+      return this.$store.state.search.page.posts;
+    }
+  }
 };
 </script>
 
