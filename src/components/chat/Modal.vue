c990349990dc49df56b30984e7f9b6672b88acf2
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-11-16 11:06:58 +0300

    chat: add scrolly to new message screen

--- src/components/chat/Modal.vue
+++ src/components/chat/Modal.vue
@@ -3,9 +3,8 @@
     <div class="popup-container chat-popup" slot="content">
       <div class="content">
         <Chat mode="modal" />
-
       </div>
-      <button type="button" class="close"></button>
+      <button type="button" class="close" @click="close"></button>
     </div>
   </Modal>
 </template>
@@ -13,9 +12,12 @@
 <script>
 import Modal from "@/components/modal/Index";
 import Chat from "./Chat";
+import ModalRouter from "@/mixins/modalRouter";
 
 export default {
   name: "ChatModal",
+
+  mixins: [ModalRouter],
 
   components: {
     Modal,
