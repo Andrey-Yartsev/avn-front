7b9c0b63f927c231231f18eea1648bbc8fe03827
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-11-21 04:45:51 +0300

    reconnect WS on disconnect

--- src/ws/index.js
+++ src/ws/index.js
@@ -13,28 +13,34 @@ const actions = {
 };
 
 export default () => {
-  const tz = moment().format("ZZ");
-  const ws = new WebSocket(process.env.VUE_APP_WS_URL);
-  ws.onopen = () => {
-    new Fingerprint({ excludeWebGL: true, excludeCanvas: true }).get(fp => {
-      ws.send(
-        JSON.stringify({
-          sess: Store.state.auth.token,
-          act: "connect",
-          fp,
-          tz
-        })
-      );
-    });
-  };
-  ws.onmessage = r => {
-    const data = JSON.parse(r.data);
-    // console.log(data);
-    const keys = Object.keys(data);
-    for (let key of keys) {
-      if (actions[key]) {
-        actions[key](data[key]);
+  const start = () => {
+    const tz = moment().format("ZZ");
+    const ws = new WebSocket(process.env.VUE_APP_WS_URL);
+    ws.onopen = () => {
+      new Fingerprint({ excludeWebGL: true, excludeCanvas: true }).get(fp => {
+        ws.send(
+          JSON.stringify({
+            sess: Store.state.auth.token,
+            act: "connect",
+            fp,
+            tz
+          })
+        );
+      });
+    };
+    ws.onmessage = r => {
+      const data = JSON.parse(r.data);
+      // console.log(data);
+      const keys = Object.keys(data);
+      for (let key of keys) {
+        if (actions[key]) {
+          actions[key](data[key]);
+        }
       }
-    }
+    };
+    ws.onclose = function() {
+      setTimeout(start, 5000);
+    };
   };
+  start();
 };
