76d2207094e35af36ff2d6b7c20efc04b5376a20
Alexei Online <alexeisevua@gmail.com>
2018-11-21 17:02:11 +0300

    Add profile actions component

--- src/components/pages/profile/Index.vue
+++ src/components/pages/profile/Index.vue
@@ -40,76 +40,10 @@
             </span>
             <span class="user-login"><a>{{ profile.username }}</a></span>
           </div>
-          <div class="row">
-            <div class="content-col">
-              <div class="profile-btns-group">
-                <div class="btns-user-activity">
-                  <router-link :class="!pageName ? 'active' : ''" :to="`/${profile.username}/posts`">
-                    <span class="value">{{ profile.postsCount }}</span>
-                    <span class="label">Posts</span>
-                  </router-link>
-                  <router-link :to="`/${profile.username}/photos`">
-                    <span class="value">{{ profile.photosCount }}</span>
-                    <span class="label">Photos</span>
-                  </router-link>
-                  <router-link :to="`/${profile.username}/videos`">
-                    <span class="value">{{ profile.videosCount }}</span>
-                    <span class="label">Videos</span>
-                  </router-link>
-                </div>
-                <div class="profile-actions" v-if="isOwner(this.profile.id)">
-                  <router-link to="/settings/profile" class="btn-edit-profile">Edit profile</router-link>
-                  <button class="btn-make-post make-post-btn" @click="openAddPostModal">New post</button>
-                </div>
-                <div class="profile-actions" v-else>
-
-                  <div class="profile-actions-tip-form" :class="{show: showTip}">
-                    <Tip
-                      :user="profile"
-                      ref="tip"
-                      @cancel="closeTip"
-                    />
-                  </div>
-
-                  <button
-                    v-if="profile.canEarn"
-                    type="button" class="profile-actions__btn profile-tip-btn selected"
-                    @click="showTip = true"
-                  >Fund</button>
-
-                  <SubscribeButton :profile="profile" @requested="subsRequested"/>
-                  <div class="subscribeView">
-                    <div
-                      v-if="profile.followedBy"
-                      @click="unfollow"
-                      class="profile-actions__btn btn-subscribe disable-state">
-                      <div class="btn-subscribe__label">
-                        Unfollow
-                      </div>
-                    </div>
-                    <div
-                      v-else
-                      @click="follow"
-                      class="profile-actions__btn btn-subscribe">
-                      <div class="btn-subscribe__label">
-                        Follow
-                      </div>
-                    </div>
-                  </div>
-                  <button
-                    v-if="profile.followedBy"
-                    @click="sendMessage"
-                    type="button" 
-                    class="profile-actions__btn profile-message-btn"
-                  >Message</button>
-                  <UserDropdown
-                    class="profile-actions__btn profile-more-functions more-functions_with-text hidden-mobile"
-                    :profile="profile"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProfileActions
+            :profile="profile"
+            :page="pageName"
+          />
         </div>
       </div>
       <div class="container">
@@ -165,13 +99,11 @@ import PostCollection from "@/components/common/postCollection/Index";
 import ProfileAvatar from "@/components/common/profile/avatar/Index";
 import InfinityScrollMixin from "@/mixins/infinityScroll";
 import UserMixin from "@/mixins/user";
-import SubscribeButton from "@/components/subscription/Button";
-import Tip from "@/components/common/tip/User";
 import FileUpload from "@/mixins/fileUpload";
-import UserDropdown from "@/components/common/userDropdawn/Index";
 import HeaderControl from "@/components/common/profile/headerControl/Index";
 import FollowersCounter from "@/components/common/profile/followersCounter/Index";
 import ProfileBackground from "@/components/common/profile/background/Index";
+import ProfileActions from "@/components/common/profile/actions/Index";
 
 export default {
   name: "ProfileHome",
@@ -181,13 +113,11 @@ export default {
   components: {
     Loader,
     PostCollection,
-    SubscribeButton,
-    Tip,
     ProfileAvatar,
-    UserDropdown,
     FollowersCounter,
     HeaderControl,
-    ProfileBackground
+    ProfileBackground,
+    ProfileActions
   },
 
   data() {
@@ -228,9 +158,6 @@ export default {
     deletedPost() {
       return this.$store.state.profile.home.deletedPost;
     },
-    funded() {
-      return this.$store.state.tip.funded;
-    },
     allMediaTypes() {
       return [...this.inputAcceptTypes.photo];
     }
@@ -244,9 +171,6 @@ export default {
     },
     deletedPost() {
       this.$store.dispatch("profile/home/fetchProfile", this.username);
-    },
-    funded() {
-      this.closeTip();
     }
   },
   methods: {
@@ -259,71 +183,20 @@ export default {
           this.getPosts();
         });
     },
-    follow() {
-      if (this.user) {
-        this.$store.dispatch("profile/home/follow", this.profile.id);
-      } else {
-        this.$router.push("/login");
-      }
-    },
-    unfollow() {
-      this.$store.dispatch("profile/home/unfollow", this.profile.id);
-    },
-    sendMessage() {
-      this.$router.push("/chat/" + this.profile.id);
+    openAddPostModal() {
+      this.$store.dispatch("modal/show", {
+        name: "addPost"
+      });
     },
     infinityScrollGetDataMethod() {
       if (this.profile) {
         this.getPosts();
       }
     },
-    subsRequested(data) {
-      if (data.action === "unsubscribe") {
-        this.unsubscribed(data.result);
-      } else if (data.action === "resubscribe") {
-        this.resubscribed(data.result);
-      } else {
-        throw new Error("Wrong action");
-      }
-    },
-    unsubscribed(result) {
-      if (!result.success) {
-        return;
-      }
-      this.$store.dispatch("profile/home/extend", {
-        subscribedByExpire: true
-      });
-      this.$store.dispatch(
-        "global/flashToast",
-        "You have unsubscribed successfully"
-      );
-    },
-    resubscribed(result) {
-      if (!result.success) {
-        return;
-      }
-      this.$store.dispatch("profile/home/extend", {
-        subscribedByExpire: false
-      });
-      this.$store.dispatch(
-        "global/flashToast",
-        "You have resubscribed successfully"
-      );
-    },
     getPosts() {
       this.$store.dispatch("profile/home/getPosts", this.profile.id);
-    },
-    openAddPostModal() {
-      this.$store.dispatch("modal/show", {
-        name: "addPost"
-      });
-    },
-    closeTip() {
-      this.showTip = false;
-      this.$refs.tip.reset();
     }
   },
-
   created() {
     this.init();
   }
