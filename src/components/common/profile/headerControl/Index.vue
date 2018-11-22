e03bb5a60c2a951bcb5cc19f170f042d5276f216
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-11-22 14:22:43 +0300

    fix back button on profile page

--- src/components/common/profile/headerControl/Index.vue
+++ src/components/common/profile/headerControl/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sticky-header-controls">
-    <a class="header-return-btn" />
+    <router-link to="/" class="header-return-btn"></router-link>
     <div class="profile-name__header">
       <span class="name">
         {{ profile.name }}
