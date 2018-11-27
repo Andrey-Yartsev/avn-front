f0fe6d1e92c9591868428dab8a2aae3ca4bcaf6c
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-11-27 17:04:30 +0300

    output search posts in columns

--- src/components/pages/search/types/Posts.vue
+++ src/components/pages/search/types/Posts.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="posts">
-    <Post
-      v-for="post in items"
-      v-bind:key="post.id"
-      :post="post"
-      from="search/page"
-    />
+    <div class="post-column">
+      <Post
+        v-for="post in posts1"
+        v-bind:key="post.id"
+        :post="post"
+        from="search/page"
+      />
+    </div>
+    <div class="post-column">
+      <Post
+        v-for="post in posts2"
+        v-bind:key="post.id"
+        :post="post"
+        from="search/page"
+      />
+    </div>
   </div>
 </template>
 
@@ -19,6 +29,15 @@ export default {
 
   components: {
     Post
+  },
+
+  computed: {
+    posts1() {
+      return this.items.filter((v, i) => !(i % 2));
+    },
+    posts2() {
+      return this.items.filter((v, i) => i % 2);
+    }
   }
 };
 </script>
