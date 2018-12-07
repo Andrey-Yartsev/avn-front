f04311777945c9bc4bdfbbe303ad2dac41998238
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-12-07 15:34:50 +0300

    hotfix

--- src/ws/actions/message.js
+++ src/ws/actions/message.js
@@ -1,11 +1,9 @@
 import Store from "@/store";
 
 export default data => {
-  // const payment = { ...Store.state.modal.payment.data };
   Store.commit("modal/hideSafe", { name: "payment" });
   if (data.success) {
     Store.dispatch("global/flashToast", `Message unlocked successful`);
-    // Store.commit("tip/funded");
   } else {
     Store.dispatch("global/flashToast", "Message unlock failed");
   }
