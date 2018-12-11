7dae0f7ce692ca521702ec1086924baed3843064
Ivan Slyusar <stompeg.yalta@gmail.com>
2018-12-11 17:44:24 +0300

    markup live unit in explore all page in stories block

--- src/components/pages/explore/Index.vue
+++ src/components/pages/explore/Index.vue
@@ -7,6 +7,22 @@
         <Navigate />
         <div class="stories-all" v-if="page === 'all' && stories.length">
           <div class="exploreAllStoriesCollectionView">
+              <div class="exploreAllStoriesView">
+                <div class="explore-all-story">
+                  <a href="/stories/467582" class="avatar with-story">
+                    <span class="avatar__slider">
+                      <span class="avatar__slide">
+                        <img src="https://media.team2.retloko.com/files/j/jf/jfk/jfkhlzooltscjljhniiux6tyx4v9ifpy1541781654/c160.jpg">
+                      </span>
+                      <span class="avatar__slide active"></span>
+                    </span>
+                    <div class="stream-online-label">live</div>
+                    <span class="live-explore-title">
+                        Top Live
+                    </span>
+                  </a>
+                </div>
+              </div>
             <VuePerfectScrollbar class="explore-stories" @ps-scroll-x="scrollFunction">
               <StorySmall v-for="post in stories" :post="post" :key="post.id" from="explore" />
             </VuePerfectScrollbar>
