22c02f729fc9e45d84ca381b66a9e07e3dfb61c1
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2018-12-13 12:38:14 +0300

    prevent scroll top the top when modal is closed

--- src/mixins/modalRouter/goto.js
+++ src/mixins/modalRouter/goto.js
@@ -14,11 +14,13 @@ export default {
       }
     },
     goToModalRoute(path) {
+      window.currentScrollTop = document.scrollingElement.scrollTop;
       path = path.replace(/^\/*(.)/, "$1");
       this.$store.dispatch("modalRouter/updatePath", path);
     },
     close() {
       window.location.hash = "";
+      document.scrollingElement.scrollTop = window.currentScrollTop;
     }
   }
 };
