c107b4abdcfc7ec5b9ce773ca84a7c984d5e05f0
alexeionline_ <alexeisevru@gmail.com>
2019-01-15 20:02:32 +0300

    fix moving between post if media is video

--- src/components/common/postParts/media/content/Video.vue
+++ src/components/common/postParts/media/content/Video.vue
@@ -6,9 +6,10 @@
     controls
     autoplay
     :poster="media.preview.source"
+    v-if="video"
   >
     <source
-      v-for="(videoSrc, videoType) in media.video"
+      v-for="(videoSrc, videoType) in video"
       :src="videoSrc"
       :type="`video/${videoType}`"
       :key="videoType"
@@ -21,6 +22,19 @@ import PostMediaPropsMixin from "@/mixins/postMedia";
 
 export default {
   name: "Video",
-  mixins: [PostMediaPropsMixin]
+  mixins: [PostMediaPropsMixin],
+  data() {
+    return {
+      video: this.media.video
+    };
+  },
+  watch: {
+    media() {
+      this.video = undefined;
+      setTimeout(() => {
+        this.video = this.media.video;
+      }, 100);
+    }
+  }
 };
 </script>
