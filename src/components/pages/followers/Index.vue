9a609a600b0749fa7c67f297e57120330a179e2c
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-01-18 14:59:49 +0300

    FIX: block "sticky-header-controls" removed from followers page

--- src/components/pages/followers/Index.vue
+++ src/components/pages/followers/Index.vue
@@ -4,7 +4,6 @@
   </div>
   <div class="profile" v-else>
     <router-link class="addPost-btn-float" to="/addPost"/>
-    <HeaderControl :profile="profile" />
     <div class="white-bg-block">
       <div class="profile-sticky-header">
         <ProfileBackground  :profile="profile" />
