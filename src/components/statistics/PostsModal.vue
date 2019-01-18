05fd8eb4a55ac47a7d2eba72b9eeb63c51d21238
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-01-18 14:22:06 +0300

    ADD: styles for block like select in statistics; styles for popup statistics details

--- src/components/statistics/PostsModal.vue
+++ src/components/statistics/PostsModal.vue
@@ -1,8 +1,35 @@
 <template>
   <Modal :onClose="close">
-    <div class="popup-container" slot="content">
+    <div class="popup-container statistics-popup" slot="content">
       <div class="content" ref="content">
-        <div class="popup-title">Posts</div>
+        <div class="popup-title popup-title_underlined">
+          Posts
+
+          <div class="more-functions more-functions_select">
+            <div class="more-functions__overlay"></div>
+            <div class="more-functions__btn">
+              <div class="more-functions__btn-text">Today</div>
+            </div>
+            <div class="more-functions__dropdown">
+              <div class="more-functions__dropdown-inside">
+                <ul>
+                  <li>
+                    <button type="button">Today</button>
+                  </li>
+                  <li>
+                    <button type="button">Last week</button>
+                  </li>
+                  <li>
+                    <button type="button">Last month</button>
+                  </li>
+                  <li>
+                    <button type="button">Last year</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
         <Posts :data="data" />
       </div>
       <button type="button" class="close" @click="close"></button>
