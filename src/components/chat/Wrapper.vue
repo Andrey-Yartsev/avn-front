df89ca6a0b7cdebc2cef31e535720a6119271be9
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-01-23 18:48:55 +0300

    chat: add no-chats class if no chats

--- src/components/chat/Wrapper.vue
+++ src/components/chat/Wrapper.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="chatCollectionContent" :class="{chat: isSecondScreen}">
-
+    <div
+      class="chatCollectionContent"
+      :class="{chat: isSecondScreen, 'no-chats': !!$store.state.chat.chats}"
+    >
       <template>
         <div class="chatHeader">
           <div class="contactsListHeader">
