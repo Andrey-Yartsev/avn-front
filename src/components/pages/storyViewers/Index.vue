27f3bb686908358984a5fd34abd86e956613fee7
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-01-24 15:19:34 +0300

    ADD: markup and styles for story details about viewer

--- src/components/pages/storyViewers/Index.vue
+++ src/components/pages/storyViewers/Index.vue
@@ -1,6 +1,108 @@
 <template>
   <div class="StoryPageCollectionView">
     <img src="http://placehold.it/300x300" />
+
+    <div class="popup">
+      <div class="overlay"></div>
+      <div class="popup-container storyviewers-popup">
+        <div class="content">
+
+          <div class="container-story-details">
+            <div class="header-story-details">
+                <span class="icn-viewer"></span>
+                4
+                2 hours
+
+                <span class="icn-remove"></span>
+            </div>
+            <div class="body-story-details">
+              <div class="list-viewers">
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+                <div class="viewer-row">
+                  <span class="avatar avatar_gap-r-sm avatar_sm"><span class="avatar__img"></span></span>
+                  <div class="username-group">
+                    <span class="name">OnMyTeam.com Support</span>
+                    <div class="user-login">
+                      <span class="username">support</span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn-block"><span class="icn-block"></span></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
