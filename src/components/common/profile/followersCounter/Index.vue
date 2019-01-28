18304fce803c7ec64029bbce0d67bd75fd3f9c32
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-01-28 16:16:50 +0300

    FIX: removed link tag for likes on profile page

--- src/components/common/profile/followersCounter/Index.vue
+++ src/components/common/profile/followersCounter/Index.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="profile-data">
     <div class="item">
-        <router-link to="/following" v-if="isOwner(profile.id)">
-            <span class="value">{{ profile.followingCount }}</span>
-            <span class="label">Following</span>
-        </router-link>
-        <template v-else>
-            <span class="value">{{ profile.followingCount }}</span>
-            <span class="label">Following</span>
-        </template>
-      </div>
-      <div class="item">
-        <router-link to="/followers" v-if="isOwner(profile.id)">
-          <span class="value">{{ profile.followersCount }}</span>
-          <span class="label">Followers</span>
-        </router-link>
-        <template v-else>
-          <span class="value">{{ profile.followersCount }}</span>
-          <span class="label">Followers</span>
-        </template>
-      </div>
-      <div class="item">
-        <a>
-          <span class="value">{{ profile.favoritesCount }}</span>
-          <span class="label">Likes</span>
-        </a>
+      <router-link to="/following" v-if="isOwner(profile.id)">
+        <span class="value">{{ profile.followingCount }}</span>
+        <span class="label">Following</span>
+      </router-link>
+      <template v-else>
+        <span class="value">{{ profile.followingCount }}</span>
+        <span class="label">Following</span>
+      </template>
+    </div>
+    <div class="item">
+      <router-link to="/followers" v-if="isOwner(profile.id)">
+        <span class="value">{{ profile.followersCount }}</span>
+        <span class="label">Followers</span>
+      </router-link>
+      <template v-else>
+        <span class="value">{{ profile.followersCount }}</span>
+        <span class="label">Followers</span>
+      </template>
+    </div>
+    <div class="item">
+      <span class="value">{{ profile.favoritesCount }}</span>
+      <span class="label">Likes</span>
     </div>
   </div>
 </template>
