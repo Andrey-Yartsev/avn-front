cec07a5896ed8ef78e17852e12ef179cc76c9afd
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-02-19 15:50:23 +0300

    chat fix

--- src/components/chat/mute.js
+++ src/components/chat/mute.js
@@ -1,6 +1,9 @@
 export default {
   methods: {
     _isMuted(user) {
+      if (!user || !user.followedOn) {
+        return false;
+      }
       if (user.followedOn && user.followedOn.isMuted) {
         return true;
       }
