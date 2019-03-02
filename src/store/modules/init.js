f2a5b642bb3654c62aa883f7dd993218a876597c
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-03-02 10:18:30 +0300

    hotfix

---
+++ src/store/modules/init.js
@@ -0,0 +1,29 @@
+"use strict";
+
+import { createRequestAction } from "../utils/storeRequest";
+
+const state = {};
+
+const actions = {};
+
+const mutations = {};
+
+createRequestAction({
+  prefix: "fetch",
+  apiPath: "init",
+  requestType: "no-token",
+  resultKey: "data",
+  state,
+  mutations,
+  actions,
+  options: {
+    method: "GET"
+  }
+});
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+};
