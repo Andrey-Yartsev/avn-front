601f071d3b9332932f5fa4c845c7ce9fc4bbcdc5
alexeionline_ <alexeisevru@gmail.com>
2019-03-20 16:12:41 +0300

    add delayed posts to modal addPost

--- src/components/addPost/Modal.vue
+++ src/components/addPost/Modal.vue
@@ -7,6 +7,14 @@
         <div class="content">
           <AddPost :initialExpanded="true" :close="close" type="new" />
         </div>
+        <template v-if="false">
+          <div class="feed reset-btr">
+            <PostCollection :posts="delayedPosts" from="postQueue" />
+          </div>
+          <div class="loaderWrap loader-content" v-if="infinityScrollLoading">
+            <Loader :fullscreen="false" />
+          </div>
+        </template>
         <button type="button" class="close" @click="close"></button>
         <div class="next hidden"></div>
       </div>
@@ -17,18 +25,38 @@
 <script>
 import Modal from "@/components/modal/Index";
 import AddPost from "./Index";
+import PostCollection from "@/components/common/postCollection/Index";
+import InfinityScrollMixin from "@/mixins/infinityScroll";
 
 export default {
   name: "AddPostModal",
+  mixins: [InfinityScrollMixin],
   components: {
     Modal,
-    AddPost
+    AddPost,
+    PostCollection
   },
   methods: {
     close(e) {
       e.preventDefault();
       this.$store.dispatch("modal/hide", { name: "addPost" });
+    },
+    infinityScrollGetDataMethod() {
+      this.$store.dispatch("postQueue/getPosts");
     }
+  },
+  computed: {
+    delayedPosts() {
+      return this.$store.state.postQueue.posts;
+    },
+    store() {
+      // uses into InfinityScrollMixin
+      return this.$store.state.postQueue;
+    }
+  },
+  created() {
+    this.$store.commit("postQueue/reset");
+    this.$store.dispatch("postQueue/getPosts");
   }
 };
 </script>
