547755b5557644f9b016c55241ccdf55bf37b031
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-03-20 15:59:40 +0300

    REF: icon live hidden for mobile

--- src/components/stream/MediumView.vue
+++ src/components/stream/MediumView.vue
@@ -6,7 +6,10 @@
     <div class="postLink live">
       <figure class="explore-media">
         <img :src="imageSrc" />
-        <div v-if="duraion" class="item-length item-length_live hidden-mobile">
+        <div
+          v-if="duraion && $mq === 'desktop'"
+          class="item-length item-length_live hidden-mobile"
+        >
           <span class="value">{{ duraion }}</span>
         </div>
         <div class="overlay">
