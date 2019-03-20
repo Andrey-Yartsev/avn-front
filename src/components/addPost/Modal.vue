3db297921e95d11debf250508af1c7387ece27dd
alexeionline_ <alexeisevru@gmail.com>
2019-03-20 20:22:56 +0300

    add infinity scroll to Share posts modal

--- src/components/addPost/Modal.vue
+++ src/components/addPost/Modal.vue
@@ -15,27 +15,26 @@
               New post
             </div>
             <AddPost :initialExpanded="true" :close="close" type="new" />
-            <template v-if="false">
-              <div class="popup-container-scroll" v-if="delayedPosts.length">
-                <VuePerfectScrollbar
-                  :settings="{ suppressScrollX: true }"
-                  class="popup-content-scroll add-post-schedule"
+            <div class="popup-container-scroll" v-if="delayedPosts.length">
+              <VuePerfectScrollbar
+                @ps-scroll-y="scrollFunction"
+                :settings="{ suppressScrollX: true }"
+                class="popup-content-scroll add-post-schedule"
+              >
+                <div class="feed reset-btr">
+                  <PostCollection :posts="delayedPosts" from="postQueue" />
+                </div>
+                <div
+                  class="loaderWrap loader-content"
+                  v-if="loading"
                 >
-                  <div class="feed reset-btr">
-                    <PostCollection :posts="delayedPosts" from="postQueue" />
-                  </div>
-                  <div
-                    class="loaderWrap loader-content"
-                    v-if="infinityScrollLoading"
-                  >
-                    <Loader :fullscreen="false" />
-                  </div>
-                </VuePerfectScrollbar>
-              </div>
-            </template>
+                  <Loader :fullscreen="false" />
+                </div>
+              </VuePerfectScrollbar>
+            </div>
           </div>
         </div>
-        <button type="button" class="close" />
+        <button type="button" class="close" @click="close"/>
         <div class="next hidden"></div>
       </div>
     </template>
@@ -46,17 +45,17 @@
 import Modal from "@/components/modal/Index";
 import AddPost from "./Index";
 import PostCollection from "@/components/common/postCollection/Index";
-import InfinityScrollMixin from "@/mixins/infinityScroll";
 import VuePerfectScrollbar from "vue-perfect-scrollbar";
+import Loader from "@/components/common/Loader";
 
 export default {
   name: "AddPostModal",
-  mixins: [InfinityScrollMixin],
   components: {
     Modal,
     AddPost,
     PostCollection,
-    VuePerfectScrollbar
+    VuePerfectScrollbar,
+    Loader
   },
   methods: {
     close(e) {
@@ -65,15 +64,25 @@ export default {
     },
     infinityScrollGetDataMethod() {
       this.$store.dispatch("postQueue/getPosts");
+    },
+    scrollFunction(e) {
+      const { scrollHeight, scrollTop, offsetHeight } = e.srcElement;
+      const scrolledEnought = scrollHeight - (offsetHeight + scrollTop) < 100;
+
+      if (scrolledEnought && !this.loading && !this.allDataReceived) {
+        this.$store.dispatch("postQueue/getPosts");
+      }
     }
   },
   computed: {
     delayedPosts() {
       return this.$store.state.postQueue.posts;
     },
-    store() {
-      // uses into InfinityScrollMixin
-      return this.$store.state.postQueue;
+    loading() {
+      return this.$store.state.postQueue.loading;
+    },
+    allDataReceived() {
+      return this.$store.state.postQueue.allDataReceived;
     }
   },
   created() {
