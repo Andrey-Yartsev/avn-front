64e493ea87908e899ac8d673120d762453321703
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-04-11 14:45:14 +0300

    ADD reserved words generator

---
+++ routes-export.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const s = fs.readFileSync("src/router/index.js").toString();
+
+const re = /path: "([^"]+)",/g;
+let m;
+const a = [];
+do {
+  m = re.exec(s);
+  if (m) {
+    if (m[1] === "/") {
+      continue;
+    }
+    a.push(m[1].replace(new RegExp("/([^/]+)/?.*"), "$1"));
+  }
+} while (m);
+
+const b = a.filter(function(item, pos, self) {
+  return self.indexOf(item) === pos;
+});
+
+fs.writeFileSync("reserved-words.json", JSON.stringify(b));
+console.log("done");
