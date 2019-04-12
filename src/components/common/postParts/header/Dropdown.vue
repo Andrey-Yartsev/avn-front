87edafe6e7a34a2e8b1db170ab0d5dc032ad1851
alexeionline_ <alexeisevru@gmail.com>
2019-04-12 19:28:47 +0300

    fix post edit url

--- src/components/common/postParts/header/Dropdown.vue
+++ src/components/common/postParts/header/Dropdown.vue
@@ -131,7 +131,7 @@ export default {
     },
 
     editPost() {
-      this.$router.push(`post/edit/${this.postId}`);
+      this.$router.push(`/post/edit/${this.postId}`);
     },
 
     reportUser() {
