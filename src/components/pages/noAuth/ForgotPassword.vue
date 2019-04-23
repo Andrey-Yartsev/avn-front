821886ffb96b6fd1a9971a8af148b83da3bd1b4a
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-04-23 11:15:04 +0300

    ADD make password restore page looks like in twitter

--- src/components/pages/noAuth/ForgotPassword.vue
+++ src/components/pages/noAuth/ForgotPassword.vue
@@ -1,39 +1,53 @@
 <template>
   <div class="forgot staticPage">
     <div class="container">
-      <h1>Reset Password</h1>
-      <h2>
-        Please fill out your email. A link to reset password will be sent there.
-      </h2>
-      <form class="forgot-password-form" v-on:submit.stop.prevent="send">
-        <div class="form-group form-group_row">
-          <span class="form-group form-group_clear-gaps">
-            <span class="form-field">
-              <input
-                v-model="email"
-                class="lg rounded"
-                type="text"
-                name="email"
-                placeholder="Your Email"
-                :disabled="loading || sendSuccess"
-              />
+      <template v-if="!sendSuccess">
+        <h1>Reset Password</h1>
+        <h2>
+          Please fill out your email. A link to reset password will be sent
+          there.
+        </h2>
+        <form class="forgot-password-form" v-on:submit.stop.prevent="send">
+          <div class="form-group form-group_row">
+            <span class="form-group form-group_clear-gaps">
+              <span class="form-field">
+                <input
+                  v-model="email"
+                  class="lg rounded"
+                  type="text"
+                  name="email"
+                  placeholder="Your Email"
+                  :disabled="loading"
+                />
+              </span>
+              <div class="error-info" v-if="error">
+                {{ error }}
+              </div>
+              <div class="success-info info">
+                Please check your mailbox for further instructions to recover
+                your password.
+              </div>
             </span>
-            <div class="error-info" v-if="error">
-              {{ error }}
-            </div>
-            <div class="success-info info" v-else-if="sendSuccess">
-              Please check your mailbox for further instructions to recover your
-              password.
-            </div>
-          </span>
-        </div>
-        <div class="g-recaptcha" id="captcha"></div>
-        <div class="form-group form-group_row">
-          <button type="submit" class="btn lg alt" v-if="!sendSuccess">
-            Reset password
-          </button>
-        </div>
-      </form>
+          </div>
+          <div class="g-recaptcha" id="captcha"></div>
+          <div class="form-group form-group_row">
+            <button type="submit" class="btn lg alt">
+              Reset password
+            </button>
+          </div>
+        </form>
+      </template>
+      <template v-else>
+        <h1>Check your email</h1>
+        <p>
+          We've sent an email to {{ maskedEmail }}. Click the link in the email
+          to reset your password.
+        </p>
+        <p>
+          If you don't see the email, check other places it might be, like your
+          junk, spam, social, or other folders.
+        </p>
+      </template>
     </div>
     <Footer class="site-footer_main" />
   </div>
@@ -67,6 +81,11 @@ export default {
         return this.$store.state.forgot.sendError.message;
       }
       return null;
+    },
+    maskedEmail() {
+      const regex = /(?<=.)[^@\n](?=[^@\n]*?@)|(?:(?<=@.)|(?!^)\\G(?=[^@\n]*$)).(?=.*\.)/gm;
+      const subst = `*`;
+      return this.email.replace(regex, subst);
     }
   },
 
