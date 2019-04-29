38790c66565b17b9692a51a7e206500576a219e5
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-04-29 16:00:32 +0300

    FIX: ability to increase height of textarea post comment added when more text inside

--- src/components/common/postParts/addNewComment/Index.vue
+++ src/components/common/postParts/addNewComment/Index.vue
@@ -1,14 +1,16 @@
 <template>
   <form class="comment-form">
-    <textarea
+    <TextareaAutosize
       class="comment-input rounded"
       placeholder="Add a comment"
       rows="1"
       v-model="message"
       ref="message"
       maxlength="200"
+      :minHeight="30"
+      :maxHeight="85"
       @keypress.enter.prevent="sendComment"
-    ></textarea>
+    ></TextareaAutosize>
     <button
       type="submit"
       class="btn comment-btn"
@@ -21,8 +23,13 @@
 </template>
 
 <script>
+import TextareaAutosize from "@/components/common/TextareaAutosize";
+
 export default {
   name: "AddComment",
+  components: {
+    TextareaAutosize
+  },
   data: () => ({
     message: ""
   }),
