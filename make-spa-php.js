b3f547e0c3307f937f6cebbc755b8abd0b97feb6
Сергей, админ <rt@aspirinka.net>
2019-05-01 15:49:17 +0000

    quick fix spa.php generation

--- make-spa-php.js
+++ make-spa-php.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 let s = fs.readFileSync("dist/index.html").toString();
 
-const meta = "<?php include __DIR__ . '/meta.php'; ?>";
+const meta = "<?php include '/var/www/stars.avn.com/current/public_html/meta.php'; ?>";
 
 s = s.replace("</head>", meta + "</head>");
 
-fs.writeFileSync("spa.php", s);
+fs.writeFileSync("dist/spa.php", s);
 console.log("spa.php generated");
