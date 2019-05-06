36d0a4b6aa74c79d870576398fedc7320dedf79c
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-05-06 16:46:50 +0300

    FIX: class tips removed from button in story view page

--- src/components/pages/story/Index.vue
+++ src/components/pages/story/Index.vue
@@ -252,7 +252,7 @@
           @click="openTip"
           v-if="!isOwner(author.id) && author.canEarn"
           type="button"
-          class="btn-tip tips"
+          class="btn-tip"
         >
           <span class="btn-icon tips" v-tooltip="'Fund'"></span>
         </button>
