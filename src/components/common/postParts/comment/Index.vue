d6b24ac3fce04b2ad865c5678e17e352c83facee
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-05-27 12:21:52 +0300

    ADD user bubble for reply usernames

--- src/components/common/postParts/comment/Index.vue
+++ src/components/common/postParts/comment/Index.vue
@@ -110,6 +110,25 @@ export default {
     hideBubble() {
       Bubble.hide();
     }
+  },
+  mounted() {
+    if (this.$mq === "mobile") {
+      return;
+    }
+    if (this.$refs.text) {
+      const aTags = this.$refs.text.getElementsByTagName("a");
+      if (aTags && aTags.length) {
+        for (let a of aTags) {
+          if (!a.innerText.match(/^@(.*)/)) {
+            continue;
+          }
+          a.addEventListener("mouseover", () => {
+            Bubble.open({ a });
+          });
+          a.addEventListener("mouseout", Bubble.hide);
+        }
+      }
+    }
   }
 };
 </script>
