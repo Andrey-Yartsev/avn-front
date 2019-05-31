3c215c6991b1440ecbeb861651ff471d05617eb4
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-05-31 12:57:06 +0300

    FIX: for button New message removed styles and used styles for default button in site

--- src/components/chat/NoConversations.vue
+++ src/components/chat/NoConversations.vue
@@ -1,7 +1,10 @@
 <template>
   <div slot="extra" class="msg-no-chat visible">
     <div class="msg-no-chat__msg">You have no active conversations</div>
-    <router-link tag="div" to="/chat/new" class="btn-start newMessageEvent"
+    <router-link
+      tag="div"
+      to="/chat/new"
+      class="btn border md alt btn-start newMessageEvent"
       >New message</router-link
     >
   </div>
