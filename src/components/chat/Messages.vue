911e9fc849335141777c2997edd8b6a363eaa062
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-06-03 12:37:11 +0300

    FIX: changes code

--- src/components/chat/Messages.vue
+++ src/components/chat/Messages.vue
@@ -75,7 +75,9 @@
                 class="timestamp timestamp_sm-size message-time"
                 v-if="v.lastMessageInGroup"
               >
-                <span class="status" v-if="isAuthor(v)">{{ v.isNew ? "Sent" : "Seen" }} </span>
+                <span class="status" v-if="isAuthor(v)"
+                  >{{ v.isNew ? "Sent" : "Seen" }}
+                </span>
                 <span class="timeValue">{{ time(v.changedAt) }}</span>
               </div>
             </div>
