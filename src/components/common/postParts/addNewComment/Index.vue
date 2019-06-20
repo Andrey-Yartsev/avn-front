a36b0acb8dd4651a85e669207302bd50c34bda8a
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-06-20 13:14:01 +0300

    ADD suggestions support for comments

--- src/components/common/postParts/addNewComment/Index.vue
+++ src/components/common/postParts/addNewComment/Index.vue
@@ -1,16 +1,19 @@
 <template>
   <form class="comment-form">
-    <TextareaAutosize
-      class="comment-input rounded"
-      placeholder="Add a comment"
-      rows="1"
-      v-model="message"
-      ref="message"
-      maxlength="200"
-      :minHeight="30"
-      :maxHeight="85"
-      @keypress.enter.prevent="sendComment"
-    />
+    <vue-tribute :options="tributeOptions">
+      <TextareaAutosize
+        id="comment_message"
+        class="comment-input rounded"
+        placeholder="Add a comment"
+        rows="1"
+        v-model="message"
+        ref="message"
+        maxlength="200"
+        :minHeight="30"
+        :maxHeight="85"
+        @keypress.enter.prevent="sendComment"
+      />
+    </vue-tribute>
     <button
       type="submit"
       class="btn comment-btn"
@@ -24,11 +27,13 @@
 
 <script>
 import TextareaAutosize from "@/components/common/TextareaAutosize";
+import VueTribute from "vue-tribute";
 
 export default {
   name: "AddComment",
   components: {
-    TextareaAutosize
+    TextareaAutosize,
+    VueTribute
   },
   data: () => ({
     message: ""
@@ -43,10 +48,54 @@ export default {
       default: ""
     }
   },
+  computed: {
+    tributeOptions() {
+      return {
+        values: this.usersSearch,
+        lookup: "name",
+        fillAttr: "name",
+        selectTemplate: function(item) {
+          if (typeof item === "undefined") {
+            return null;
+          }
+          return "@" + item.original.username;
+        },
+        menuItemTemplate: function(item) {
+          let template = ``;
+          template += `<span contenteditable="false">
+<a tabindex="2" class="user">
+  <span class="avatar avatar_gap-r-sm avatar_sm">
+    <span class="avatar__img">`;
+          if (item.original.avatar) {
+            template += `<img src="${item.original.avatar}">`;
+          }
+          template += `
+    </span>
+  </span>
+  <div class="username-group">
+    <div class="user__name">
+      <div class="name">${item.original.name}</div>
+      <span class="verified-user icn-item"></span>
+    </div>
+    <span class="user-login reset-ml">${item.original.username}</span>
+  </div>
+</a>
+</span>
+`;
+          return template;
+        }
+      };
+    }
+  },
   methods: {
     sendComment() {
       this.sendNewComment(this.message);
       this.message = "";
+    },
+    usersSearch(text, cb) {
+      this.$store.dispatch("search/tribute/search", text).then(result => {
+        cb(result.list);
+      });
     }
   },
   watch: {
@@ -54,6 +103,13 @@ export default {
       this.message = `@${this.userName} `;
       this.$refs.message.$el.focus();
     }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$refs.message.$el.addEventListener("tribute-replaced", () => {
+        this.message = this.$refs.message.$el.value;
+      });
+    }, 0);
   }
 };
 </script>
