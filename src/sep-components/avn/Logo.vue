63fd92ae221a72418e8867b11f346fa8c9342c9c
alexeionline_ <alexeisevru@gmail.com>
2019-06-21 15:23:31 +0300

    update logo link

--- src/sep-components/avn/Logo.vue
+++ src/sep-components/avn/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <a href="/">
+    <router-link to="/">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54 51">
         <g id="Layer_2" data-name="Layer 2">
           <g id="Layer_1-2" data-name="Layer 1">
@@ -30,6 +30,6 @@
           </g>
         </g>
       </svg>
-    </a>
+    </router-link>
   </div>
 </template>
