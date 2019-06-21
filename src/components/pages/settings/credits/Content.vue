d1e67e1fe9ea1d8ae832ebaa60a1da5034870ce4
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-06-21 15:21:54 +0300

    FIX: styles and markup for form field with symbol $ changed

--- src/components/pages/settings/credits/Content.vue
+++ src/components/pages/settings/credits/Content.vue
@@ -18,11 +18,13 @@
                 </span>
                 <span class="form-group form-group_clear-gaps">
                   <span class="form-field field-symbol-currency">
-                    <input
-                      name="amount"
-                      v-model="amount"
-                      class="field-gap_currency"
-                    />
+                    <span class="form-field">
+                      <input
+                        name="amount"
+                        v-model="amount"
+                        class="field-gap_currency"
+                      />
+                    </span>
                   </span>
                 </span>
               </label>
