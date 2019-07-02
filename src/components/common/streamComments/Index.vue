4e1d16a3c2082c7236435cce55a03e44a7e10947
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-07-02 13:07:10 +0300

    FIX: markup for viewers, payer, likers hidden in stream page

--- src/components/common/streamComments/Index.vue
+++ src/components/common/streamComments/Index.vue
@@ -7,57 +7,59 @@
         :comment="comment"
       />
 
-      <div class="comment stream-message">
-        <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
-          <span class="avatar__img">
-            <img
-              src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
-            />
+      <template v-if="false">
+        <div class="comment stream-message">
+          <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
+            <span class="avatar__img">
+              <img
+                src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
+              />
+            </span>
           </span>
-        </span>
-        <div class="comment-content stream-message__head">
-          <div class="comment-body">
-            <span class="name">tester11</span>
-            <span class="comment-text stream-message__text"
-              ><span class="likes icn-item icn-size_md"></span> 10</span
-            >
+          <div class="comment-content stream-message__head">
+            <div class="comment-body">
+              <span class="name">tester11</span>
+              <span class="comment-text stream-message__text"
+                ><span class="likes icn-item icn-size_md"></span> 10</span
+              >
+            </div>
           </div>
         </div>
-      </div>
-      <div class="comment stream-message">
-        <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
-          <span class="avatar__img">
-            <img
-              src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
-            />
+        <div class="comment stream-message">
+          <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
+            <span class="avatar__img">
+              <img
+                src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
+              />
+            </span>
           </span>
-        </span>
-        <div class="comment-content stream-message__head">
-          <div class="comment-body">
-            <span class="name">tester11</span>
-            <span class="comment-text stream-message__text"
-              ><span class="icn-tips icn-item icn-size_md"></span> 10</span
-            >
+          <div class="comment-content stream-message__head">
+            <div class="comment-body">
+              <span class="name">tester11</span>
+              <span class="comment-text stream-message__text"
+                ><span class="icn-tips icn-item icn-size_md"></span> 10</span
+              >
+            </div>
           </div>
         </div>
-      </div>
-      <div class="comment stream-message">
-        <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
-          <span class="avatar__img">
-            <img
-              src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
-            />
+        <div class="comment stream-message">
+          <span class="avatar avatar_not-shadow avatar_ex-sm avatar_gap-r-sm">
+            <span class="avatar__img">
+              <img
+                src="https://avnsocial-dev.s3.amazonaws.com/files/h/hv/hvt/hvtdzqnjkwmc3u3rf3ylcnn6gdhohf6n1555004518/avatar.jpg"
+              />
+            </span>
           </span>
-        </span>
-        <div class="comment-content stream-message__head">
-          <div class="comment-body">
-            <span class="name">tester11</span>
-            <span class="comment-text stream-message__text"
-              ><span class="looking icn-item icn-size_md"></span
-            ></span>
+          <div class="comment-content stream-message__head">
+            <div class="comment-body">
+              <span class="name">tester11</span>
+              <span class="comment-text stream-message__text"
+                ><span class="looking icn-item icn-size_md"></span
+              ></span>
+            </div>
           </div>
         </div>
-      </div>
+      </template>
     </perfect-scrollbar>
   </div>
 </template>
