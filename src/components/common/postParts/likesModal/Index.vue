ebd31fd36e5fa9389b872c12d72c32380ada9b75
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-07-05 18:57:21 +0300

    FIX: npm lint changes code

--- src/components/common/postParts/likesModal/Index.vue
+++ src/components/common/postParts/likesModal/Index.vue
@@ -12,7 +12,10 @@
                 class="ps"
               >
                 <Item v-for="user in users" :key="user.id" :user="user" />
-                <div class="loaderWrap loader-content" v-if="infinityScrollLoading">
+                <div
+                  class="loaderWrap loader-content"
+                  v-if="infinityScrollLoading"
+                >
                   <Loader :fullscreen="false" />
                 </div>
               </perfect-scrollbar>
