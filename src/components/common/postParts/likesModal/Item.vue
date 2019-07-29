c9a3f29c8138054e9aeda7b743289c031ae087ed
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-07-29 17:54:10 +0300

    FIX: all row with user data made link in popup with list likers

--- src/components/common/postParts/likesModal/Item.vue
+++ src/components/common/postParts/likesModal/Item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="viewer-row viewer-row_likers">
+  <router-link :to="'/' + user.username" class="viewer-row viewer-row_likers">
     <span class="avatar avatar_gap-r-sm avatar_sm">
       <span class="avatar__img">
         <img :src="user.avatar" />
@@ -7,16 +7,14 @@
     </span>
     <div class="username-group">
       <span class="name">
-        <router-link :to="'/' + user.username">{{ user.name }}</router-link>
+        {{ user.name }}
       </span>
       <div class="user-login reset-ml">
-        <router-link :to="'/' + user.username" class="username">{{
-          user.username
-        }}</router-link>
+        <span class="username">{{ user.username }}</span>
       </div>
     </div>
     <!-- <button class="btn sm btn-follow-viewers">Follow</button> -->
-  </div>
+  </router-link>
 </template>
 
 <script>
