50bf678fbdf169f570129cc7473ccca6e4e42340
Ivan Slyusar <stompeg.yalta@gmail.com>
2019-09-09 18:49:02 +0300

    FIX: genders list changes

--- src/components/pages/explore/navigate/Index.vue
+++ src/components/pages/explore/navigate/Index.vue
@@ -1,43 +1,41 @@
 <template>
-  <div class="content-nav-wrapper">
-    <nav class="content-nav">
-      <router-link to="/explore/all" data-type="all" class="content-nav__item">
-        <span class="hidden-mobile">Explore all</span>
-        <span class="hidden-desktop">All</span>
-      </router-link>
-      <router-link
-        to="/explore/stories"
-        class="content-nav__item"
-        data-type="stories"
-        >Stories</router-link
-      >
-      <router-link
-        to="/explore/videos"
-        class="content-nav__item"
-        data-type="videos"
-        >Videos</router-link
-      >
-      <router-link
-        to="/explore/photos"
-        class="content-nav__item"
-        data-type="photos"
-        >Photos</router-link
-      >
-      <router-link to="/explore/live" class="content-nav__item" data-type="live"
-        >Live</router-link
-      >
-      <router-link to="/explore/top" class="content-nav__item" data-type="top"
-        >Top
-        <template v-if="$mq === 'desktop'"
-          >Models</template
-        ></router-link
-      >
-      <router-link to="/explore" exact class="content-nav__item" data-type="top"
-        >Feed</router-link
-      >
-    </nav>
+  <nav class="content-nav">
+    <router-link to="/explore/all" data-type="all" class="content-nav__item">
+      <span class="hidden-mobile">Explore all</span>
+      <span class="hidden-desktop">All</span>
+    </router-link>
+    <router-link
+      to="/explore/stories"
+      class="content-nav__item"
+      data-type="stories"
+      >Stories</router-link
+    >
+    <router-link
+      to="/explore/videos"
+      class="content-nav__item"
+      data-type="videos"
+      >Videos</router-link
+    >
+    <router-link
+      to="/explore/photos"
+      class="content-nav__item"
+      data-type="photos"
+      >Photos</router-link
+    >
+    <router-link to="/explore/live" class="content-nav__item" data-type="live"
+      >Live</router-link
+    >
+    <router-link to="/explore/top" class="content-nav__item" data-type="top"
+      >Top
+      <template v-if="$mq === 'desktop'"
+        >Models</template
+      ></router-link
+    >
+    <router-link to="/explore" exact class="content-nav__item" data-type="top"
+      >Feed</router-link
+    >
     <GenderFilter v-if="$mq === 'desktop'" />
-  </div>
+  </nav>
 </template>
 
 <script>
