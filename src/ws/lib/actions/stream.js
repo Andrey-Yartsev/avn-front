2d0e96b1e30a0ec7f9d4a32b0d4e202787d037a7
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-09-09 17:24:26 +0300

    FIX filter ws actions for starting tream with gender filter

--- src/ws/lib/actions/stream.js
+++ src/ws/lib/actions/stream.js
@@ -1,7 +1,13 @@
 import Store from "@/store";
 
 export default data => {
-  Store.commit("lives/addNewOne", data);
+  const s = Store.state;
+  if (
+    s.explore.gender.category === 1 ||
+    s.explore.gender.category === data.category
+  ) {
+    Store.commit("lives/addNewOne", data);
+  }
   Store.commit("profile/home/setLive", {
     id: data.user.id,
     currentStream: data
--- src/ws/lib/actions/stream_public.js
+++ src/ws/lib/actions/stream_public.js
@@ -1,7 +1,13 @@
 import Store from "@/store";
 
 export default data => {
-  Store.commit("lives/addNewOne", { ...data, stream_public: true });
+  const s = Store.state;
+  if (
+    s.explore.gender.category === 1 ||
+    s.explore.gender.category === data.category
+  ) {
+    Store.commit("lives/addNewOne", { ...data, stream_public: true });
+  }
   Store.commit("profile/home/setLive", {
     id: data.user.id,
     currentStream: data
