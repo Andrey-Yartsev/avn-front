104fe12b544ff82968c4245fbccf505f01a36c96
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-10-03 16:16:48 +0300

    FIX show stream tips for author

--- src/ws/lib/actions/stream_tip.js
+++ src/ws/lib/actions/stream_tip.js
@@ -1,7 +1,5 @@
 import Store from "@/store";
 
 export default tip => {
-  if (Store.state.auth.user.id !== tip.user.id) {
-    Store.dispatch("lives/tip", tip);
-  }
+  Store.dispatch("lives/tip", tip);
 };
