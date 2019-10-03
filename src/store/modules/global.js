15b2867a77b1aaf318f62e82b994c4a17542832f
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-10-03 14:21:39 +0300

    ignore Canceled error 2

--- src/store/modules/global.js
+++ src/store/modules/global.js
@@ -25,7 +25,7 @@ const actions = {
   },
 
   setError({ commit }, error) {
-    if (error.message === "Canceled") {
+    if (error.message === "cancelled") {
       return;
     }
     commit("setError", error);
