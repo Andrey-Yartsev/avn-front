81604167718d4f588bb233269b716362eece7c1d
ar.bylym <ar.bylym07@gmail.com>
2019-10-10 11:59:56 +0300

    Fix error message when use login

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -45,7 +45,13 @@ const loginAction = (commit, dispatch, rootState, data, fromModal) => {
           });
         }
         dispatch("profile/afterLogin", null, { root: true });
-        dispatch("awards/nominate", rootState.awards.savedData, { root: true });
+        if (rootState.awards.savedData) {
+          dispatch("awards/nominate", rootState.awards.savedData, {
+            root: true
+          }).then(() => {
+            commit("awards/clearSavedData", null, { root: true });
+          });
+        }
       });
     })
     .catch(error => {
