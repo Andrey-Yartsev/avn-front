eecd57c637890adbde9f85790b42007c6c791cd9
ar.bylym <ar.bylym07@gmail.com>
2019-10-17 12:21:29 +0300

    Del span from header

--- src/components/addLink/Form.vue
+++ src/components/addLink/Form.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="addPost truncated">
     <div class="addPost-header" v-if="$mq === 'mobile'">
-      <button type="button" class="header-return-btn go-back go-back_times">
-        <span class="close__linkModal" @click="closeHandler" />
+      <button
+        @click="closeHandler"
+        type="button"
+        class="header-return-btn go-back go-back_times"
+      >
         <h1 class="category-name">
           {{ this.isNew ? "New Link" : "Edit Link" }}
         </h1>
