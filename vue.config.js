dbb6d3bf6d78acc50fc4d1fb6e249e0bb05dcc33
ar.bylym <ar.bylym07@gmail.com>
2019-10-24 12:55:33 +0300

    Remove analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
-
 module.exports = {
   runtimeCompiler: true,
   devServer: {
     disableHostCheck: true
-  },
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin({ openAnalyzer: false })]
   }
 };
