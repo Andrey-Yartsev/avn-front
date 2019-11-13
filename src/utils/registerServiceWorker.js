0fd7440aac4de20c8ea70b56499cee5f7e7bc16f
ar.bylym <ar.bylym07@gmail.com>
2019-11-13 09:58:19 +0200

    Fix: serviceWorker not run for Safari

--- src/utils/registerServiceWorker.js
+++ src/utils/registerServiceWorker.js
@@ -1,11 +1,16 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
+function isSafari() {
+  const is_safari = navigator.userAgent.toLowerCase().indexOf("safari/") > -1;
+  return is_safari;
+}
 
-  // navigator.serviceWorker.register("/serviceWorker.js").then(function() {
-  //   console.log("Service worker registered!");
+if ("serviceWorker" in navigator && !isSafari()) {
+  // navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  //   for (let registration of registrations) {
+  //     registration.unregister();
+  //   }
   // });
+
+  navigator.serviceWorker.register("/serviceWorker.js").then(function() {
+    console.log("Service worker registered!");
+  });
 }
