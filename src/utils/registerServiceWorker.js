8c52c510fc2728fa5b3b9f345906b1356a7ce8c7
ar.bylym <ar.bylym07@gmail.com>
2019-11-13 19:18:29 +0200

    Remove sw

--- src/utils/registerServiceWorker.js
+++ src/utils/registerServiceWorker.js
@@ -1,3 +1,12 @@
-if ("serviceWorker" in navigator && "PushManager" in window) {
-  navigator.serviceWorker.register("/sw.push.js");
+// function isSafari() {
+//   const is_safari = navigator.userAgent.toLowerCase().indexOf("safari/") > -1;
+//   return is_safari;
+// }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
 }
