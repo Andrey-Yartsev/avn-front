eeaed4092b75a9123267bf48a55ec51941c2bdc2
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2019-12-10 23:05:56 +0300

    FIX new voting links

--- src/components/common/nominationSettingsRoutes.js
+++ src/components/common/nominationSettingsRoutes.js
@@ -1,6 +1,6 @@
 import Store from "@/store";
 
-export default () => {
+export default user => {
   const items = [];
   // if (!user.adminReturnUrl) {
   //   return items;
@@ -13,11 +13,18 @@ export default () => {
   //   };
   //   items.push(a);
   // }
-  if (Store.state.init.data.enableVoting) {
+
+  if (user.nominee) {
     items.push({
       name: "avn_awards/voting",
-      path: "/avn_awards/voting",
+      path: "/settings/avn",
       title: "AVN Awards Promo Link"
+    });
+  }
+  if (Store.state.init.data.enableVoting) {
+    items.push({
+      path: "/avn_awards/voting",
+      title: "AVN Awards Voting"
     });
   }
   return items;
