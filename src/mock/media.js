b6f6c46d43ceb6a2bb6d954a31c9328346ec164c
ar.bylym <ar.bylym07@gmail.com>
2020-01-09 10:34:39 +0200

    Move mock data apart

---
+++ src/mock/media.js
@@ -0,0 +1,293 @@
+export default [
+  {
+    productId: 1178,
+    price: 12.3,
+    active: true,
+    title: "123 sodfsdf sfsdfs sdfksjadfls sfsdfsdf sdflksdfs sdfsdf sdfsdfsfs",
+    text: "fgdfgdfgfdg",
+    maxPrice: "500",
+    buyCount: 0,
+    author: {
+      id: 468788,
+      name: "имя",
+      username: "user666",
+      avatar:
+        "https://avnsocial-dev.s3.amazonaws.com/files/i/if/ifz/ifzfcvs2l2uymmy6da7928egvdhnxh4f1575995191/avatar.jpg",
+      header:
+        "https://avnsocial-dev.s3.amazonaws.com/files/a/aw/awv/awvidf0aytegt7hse0iz8gybr5xhjslv1575995230/w500.jpg",
+      headerFull:
+        "https://avnsocial-dev.s3.amazonaws.com/files/b/b0/b02/b02ovsj95uucdwr7b0x6efcioajkdfw21575995228/header.jpg",
+      canEarn: true,
+      deleteRequested: false,
+      hideBanking: false,
+      publicUrl: "https://avn2.retloko.com/user666",
+      hasNotViewedStory: false,
+      hasStory: false,
+      isVerified: false,
+      storeEnabled: true,
+      isBlocked: false,
+      isMuted: false,
+      isSubscribed: true,
+      subscribePrice: "10.00",
+      canPayoutsRequest: true,
+      nominatable: true,
+      category: 3,
+      categoryView: 1,
+      payments: {
+        tipsLimit: {
+          min: 2,
+          max: 500
+        }
+      }
+    },
+    media: {
+      id: 187,
+      canView: true,
+      type: "video",
+      duration: 31,
+      video: null,
+      background:
+        "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAQDAwQDAwQEBAQFBQQFBwsHBwYGBw4KCggLEA4RERAOEA8SFBoWEhMYEw8QFh8XGBsbHR0dERYgIh8cIhocHRz/2wBDAQUFBQcGBw0HBw0cEhASHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBz/wAARCAAdADIDASIAAhEBAxEB/8QAGQAAAwEBAQAAAAAAAAAAAAAAAgMEAAEI/8QAGBABAQEBAQAAAAAAAAAAAAAAAAIBERL/xAAXAQEBAQEAAAAAAAAAAAAAAAACAAED/8QAFhEBAQEAAAAAAAAAAAAAAAAAAAER/9oADAMBAAIRAxEAPwDwLOHTAIxTGHIIPAKlVwq8PFqWsL06yac7Gh4zrCtPjVEUknTpopUp9FXrnreF1p6zAXpWjrS9C1rdYLCsf//Z",
+      locked: null,
+      src: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/2019_12_18/5d/5df/5dfa2c081596a17fb65ff.mp4",
+        width: 480,
+        height: 270
+      },
+      preview: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/2019_12_05/5d/5de/5de8d37bbc3a97e9663cb.mp4",
+        width: 480,
+        height: 270
+      },
+      thumb: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/1/1e/1e27b0eb09f36fb2d57a570257fc5410/440x440_6490cbc43582eb029d34add601e520bd39801924_preview.jpg",
+        width: 440,
+        height: 440
+      },
+      thumbId: 2,
+      thumbs: [
+        {
+          id: 1,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 2,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 3,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 4,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 5,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        }
+      ]
+    }
+  },
+  {
+    productId: 1179,
+    price: 10.5,
+    active: true,
+    title: "123 sodfsdf sfsdfs sdfksjadfls sfsdfsdf sdflksdfs sdfsdf sdfsdfsfs",
+    text: "fgdfgdfgfdg",
+    maxPrice: "500",
+    buyCount: 0,
+    author: {
+      id: 468788,
+      name: "имя",
+      username: "user666",
+      avatar:
+        "https://avnsocial-dev.s3.amazonaws.com/files/i/if/ifz/ifzfcvs2l2uymmy6da7928egvdhnxh4f1575995191/avatar.jpg",
+      header:
+        "https://avnsocial-dev.s3.amazonaws.com/files/a/aw/awv/awvidf0aytegt7hse0iz8gybr5xhjslv1575995230/w500.jpg",
+      headerFull:
+        "https://avnsocial-dev.s3.amazonaws.com/files/b/b0/b02/b02ovsj95uucdwr7b0x6efcioajkdfw21575995228/header.jpg",
+      canEarn: true,
+      deleteRequested: false,
+      hideBanking: false,
+      publicUrl: "https://avn2.retloko.com/user666",
+      hasNotViewedStory: false,
+      hasStory: false,
+      isVerified: false,
+      storeEnabled: true,
+      isBlocked: false,
+      isMuted: false,
+      isSubscribed: true,
+      subscribePrice: "10.00",
+      canPayoutsRequest: true,
+      nominatable: true,
+      category: 3,
+      categoryView: 1,
+      payments: {
+        tipsLimit: {
+          min: 2,
+          max: 500
+        }
+      }
+    },
+    media: {
+      id: 188,
+      canView: false,
+      type: "video",
+      duration: 31,
+      video: null,
+      background:
+        "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAQDAwQDAwQEBAQFBQQFBwsHBwYGBw4KCggLEA4RERAOEA8SFBoWEhMYEw8QFh8XGBsbHR0dERYgIh8cIhocHRz/2wBDAQUFBQcGBw0HBw0cEhASHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBz/wAARCAAdADIDASIAAhEBAxEB/8QAGQAAAwEBAQAAAAAAAAAAAAAAAgMEAAEI/8QAGBABAQEBAQAAAAAAAAAAAAAAAAIBERL/xAAXAQEBAQEAAAAAAAAAAAAAAAACAAED/8QAFhEBAQEAAAAAAAAAAAAAAAAAAAER/9oADAMBAAIRAxEAPwDwLOHTAIxTGHIIPAKlVwq8PFqWsL06yac7Gh4zrCtPjVEUknTpopUp9FXrnreF1p6zAXpWjrS9C1rdYLCsf//Z",
+      locked: null,
+      src: {
+        source: null,
+        width: 480,
+        height: 270
+      },
+      preview: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/2019_12_18/5d/5df/5dfa2c081596a17fb65ff.mp4",
+        width: 480,
+        height: 270
+      },
+      thumb: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/1/1e/1e27b0eb09f36fb2d57a570257fc5410/440x440_6490cbc43582eb029d34add601e520bd39801924_preview.jpg",
+        width: 440,
+        height: 440
+      },
+      thumbId: 2,
+      thumbs: [
+        {
+          id: 1,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 2,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 3,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 4,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 5,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        }
+      ]
+    }
+  },
+  {
+    productId: 1180,
+    price: 5.0,
+    active: true,
+    title: "123 sodfsdf sfsdfs sdfksjadfls sfsdfsdf sdflksdfs sdfsdf sdfsdfsfs",
+    text: "fgdfgdfgfdg",
+    maxPrice: "500",
+    buyCount: 0,
+    author: {
+      id: 468788,
+      name: "имя",
+      username: "user666",
+      avatar:
+        "https://avnsocial-dev.s3.amazonaws.com/files/i/if/ifz/ifzfcvs2l2uymmy6da7928egvdhnxh4f1575995191/avatar.jpg",
+      header:
+        "https://avnsocial-dev.s3.amazonaws.com/files/a/aw/awv/awvidf0aytegt7hse0iz8gybr5xhjslv1575995230/w500.jpg",
+      headerFull:
+        "https://avnsocial-dev.s3.amazonaws.com/files/b/b0/b02/b02ovsj95uucdwr7b0x6efcioajkdfw21575995228/header.jpg",
+      canEarn: true,
+      deleteRequested: false,
+      hideBanking: false,
+      publicUrl: "https://avn2.retloko.com/user666",
+      hasNotViewedStory: false,
+      hasStory: false,
+      isVerified: false,
+      storeEnabled: true,
+      isBlocked: false,
+      isMuted: false,
+      isSubscribed: true,
+      subscribePrice: "10.00",
+      canPayoutsRequest: true,
+      nominatable: true,
+      category: 3,
+      categoryView: 1,
+      payments: {
+        tipsLimit: {
+          min: 2,
+          max: 500
+        }
+      }
+    },
+    media: {
+      id: 189,
+      canView: false,
+      type: "video",
+      duration: 31,
+      video: null,
+      background:
+        "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAQDAwQDAwQEBAQFBQQFBwsHBwYGBw4KCggLEA4RERAOEA8SFBoWEhMYEw8QFh8XGBsbHR0dERYgIh8cIhocHRz/2wBDAQUFBQcGBw0HBw0cEhASHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBwcHBz/wAARCAAdADIDASIAAhEBAxEB/8QAGQAAAwEBAQAAAAAAAAAAAAAAAgMEAAEI/8QAGBABAQEBAQAAAAAAAAAAAAAAAAIBERL/xAAXAQEBAQEAAAAAAAAAAAAAAAACAAED/8QAFhEBAQEAAAAAAAAAAAAAAAAAAAER/9oADAMBAAIRAxEAPwDwLOHTAIxTGHIIPAKlVwq8PFqWsL06yac7Gh4zrCtPjVEUknTpopUp9FXrnreF1p6zAXpWjrS9C1rdYLCsf//Z",
+      locked: null,
+      src: {
+        source: null,
+        width: 480,
+        height: 270
+      },
+      preview: {
+        source: null,
+        width: 480,
+        height: 270
+      },
+      thumb: {
+        source:
+          "https://avnsocial-dev.s3.amazonaws.com/files/1/1e/1e27b0eb09f36fb2d57a570257fc5410/440x440_6490cbc43582eb029d34add601e520bd39801924_preview.jpg",
+        width: 440,
+        height: 440
+      },
+      thumbId: 2,
+      thumbs: [
+        {
+          id: 1,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 2,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 3,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 4,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        },
+        {
+          id: 5,
+          url:
+            "https://avnsocial-dev.s3.amazonaws.com/files/7/7f/7fcbcb63520621df408473d15c9528f5/440x440_7d2d7d05568e671ecfddf037cd875a111050266_preview.jpg"
+        }
+      ]
+    }
+  }
+];
