d701011730a0e39ece20ca3f5a3a7f1fc6e74d19
ar.bylym <ar.bylym07@gmail.com>
2020-01-27 11:16:21 +0200

    Change webpack config to use [chunkhash] in prod build

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,15 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  publicPath: process.env.VUE_APP_CDN_PATH
+  publicPath: process.env.VUE_APP_CDN_PATH,
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        output: {
+          filename: "js/[name].[chunkhash:8].js",
+          chunkFilename: "js/[name].[chunkhash:8].js"
+        }
+      };
+    }
+  }
 };
