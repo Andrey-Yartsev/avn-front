b15b87fdbceb1f6f1ab3eece1db81b1f1ae92ec5
ar.bylym <ar.bylym07@gmail.com>
2020-02-28 11:43:36 +0200

    Redirect to modal for posts

--- src/router/middlewares/post.js
+++ src/router/middlewares/post.js
@@ -5,11 +5,15 @@ import Auth from "./auth";
 
 const Post = {
   page(to, from, next) {
+    const { username, postId } = to.params;
+    if (username && postId) {
+      next(`/${username}/posts#m/post/${postId}/profile/home`);
+    }
     Auth.requireAny(to, from, () => {
       Store.commit("postPage/resetPageState");
       Store.dispatch("postPage/getPost", { postId: to.params.postId })
-        .then(() => {
-          next();
+        .then(res => {
+          next(`/${res.author.username}/posts#m/post/${postId}/profile/home`);
         })
         .catch(() => {
           next("/not-found");
