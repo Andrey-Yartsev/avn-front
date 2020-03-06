ec258e1e6a460f8111695376779ed817f227b1f7
ar.bylym <ar.bylym07@gmail.com>
2020-03-06 18:03:25 +0200

    Add more instructions to password-confirmed page

--- src/components/pages/passwordConfirmed/Index.vue
+++ src/components/pages/passwordConfirmed/Index.vue
@@ -4,6 +4,13 @@
       <h3 class="error404__title ">
         This link for password resetting has already been used
       </h3>
+      <p>
+        Please, make sure that you don't try to use the same link to reset your
+        password several times
+      </p>
+      <router-link class="resetPasswordLink" to="/forgot"
+        >Reset password one more time</router-link
+      >
       <router-link class="btn btn_fix-width-ex-lg lg alt" to="/">
         Go back to {{ host }}
       </router-link>
@@ -33,10 +40,13 @@ export default {
   justify-content: center;
   .container {
     display: flex;
-    flex-grow: 1;
     flex-flow: column nowrap;
     align-items: center;
     justify-content: center;
   }
+}
+.resetPasswordLink {
+  margin: 10px auto;
+  padding-bottom: 10px;
 }
 </style>
