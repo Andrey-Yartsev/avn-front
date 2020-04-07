732d1bafc7ef525b6d664ef670f6eaff6d579eec
ar.bylym <ar.bylym07@gmail.com>
2020-04-07 19:33:33 +0300

    Update user profile by subscription success socket message

--- src/ws/lib/actions/subscription.js
+++ src/ws/lib/actions/subscription.js
@@ -1,36 +1,63 @@
 import Store from "@/store";
 
 export default data => {
-  const payment = { ...Store.state.modal.payment.data };
-  Store.commit("modal/hideSafe", { name: "payment" });
-  if (data.success) {
-    Store.dispatch("global/flashToast", {
-      text: `Subscription to ${payment.user.name} is successful`
-    });
+  // const payment = { ...Store.state.modal.payment.data };
+  // Store.commit("modal/hideSafe", { name: "payment" });
+  if (data.success && window.MyApp.$route.name === "ProfilePage") {
     if (Store.state.profile.home.profile) {
-      if (Store.state.profile.home.profile.id === payment.user.id) {
-        Store.dispatch("profile/home/extend", {
-          subscribedBy: true
+      if (Store.state.profile.home.profile.id === data.subscribedOn) {
+        Store.dispatch("global/flashToast", {
+          text: `Subscription to ${data.name} is successful`
         });
-        Store.commit("profile/home/resetPosts");
-        Store.dispatch("profile/home/getPosts");
+        Store.dispatch(
+          "profile/home/extend",
+          {
+            subscribedBy: true,
+            subscribedByProgress: false,
+            isPrivatePost: false
+          },
+          { root: true }
+        );
+        Store.commit("profile/home/resetPosts", null, { root: true });
+        Store.dispatch("profile/home/getPosts", null, { root: true });
+        Store.commit("highlights/resetPageState", null, { root: true });
+        Store.dispatch(
+          "highlights/setSource",
+          { source: Store.state.profile.home.profile.id },
+          { root: true }
+        );
+        Store.dispatch("highlights/getPosts", null, { root: true });
       }
     }
-    if (!payment.actionPrefix) {
-      console.log("BAD");
-      console.trace(payment);
-    }
-    Store.commit(payment.actionPrefix + "/extendUser", {
-      userId: payment.user.id,
-      data: {
-        subscribedBy: true,
-        subscribedByProgress: false
-      }
-    });
+    Store.commit(
+      "subscription/statusUpdate",
+      {
+        action: "subscribe",
+        result: {
+          success: true
+        },
+        data: {
+          userId: data.subscribedOn
+        }
+      },
+      { root: true }
+    );
+
+    // if (!payment.actionPrefix) {
+    //   console.log("BAD");
+    //   console.trace(payment);
+    // }
+    // Store.commit(payment.actionPrefix + "/extendUser", {
+    //   userId: payment.user.id,
+    //   data: {
+    //     subscribedBy: true,
+    //     subscribedByProgress: false
+    //   }
+    // });
   } else {
-    Store.dispatch("global/flashToast", {
-      text: "Subscription failed",
-      type: "error"
-    });
+    // Store.dispatch("global/flashToast", {
+    //   text: "Subscription failed",
+    //   type: "error"
+    // });
   }
 };
