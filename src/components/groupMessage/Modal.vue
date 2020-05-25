d9e1d81bb0d3ad975c928e20d068ac0928ff18d1
ar.bylym <ar.bylym07@gmail.com>
2020-05-25 15:23:21 +0300

    Update scheduled messages response format

--- src/components/groupMessage/Modal.vue
+++ src/components/groupMessage/Modal.vue
@@ -218,7 +218,7 @@ export default {
       this.$store
         .dispatch("chat/getScheduledGroupMessages", null, { root: true })
         .then(res => {
-          this.scheduledMessages = res;
+          this.scheduledMessages = res.list;
         });
     },
     deleteScheduleMessage(messageId) {
