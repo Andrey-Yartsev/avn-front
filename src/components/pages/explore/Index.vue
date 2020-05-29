16662cfb52f7de2e02542d464091bf3b5afec3ba
ar.bylym <ar.bylym07@gmail.com>
2020-05-29 16:15:25 +0300

    Revert "View lives and stories in a row"
    This reverts commit 2874725cd57fb11daa26d6d0d4d41359bb02ac39.

--- src/components/pages/explore/Index.vue
+++ src/components/pages/explore/Index.vue
@@ -41,14 +41,12 @@
           <div class="storyCollectionView storyCollectionView_tape">
             <div class="stories-group__outer">
               <div class="stories-group">
+                <!-- <TopLives :lives="streamLives" v-if="streamLives.length" /> -->
+                <TopLivesList :lives="streamLives" v-if="streamLives.length" />
                 <perfect-scrollbar
                   class="stories-group__inner"
                   @ps-scroll-x="scrollFunction"
                 >
-                  <TopLivesList
-                    :lives="streamLives"
-                    v-if="streamLives.length"
-                  />
                   <StorySmall
                     v-for="post in stories"
                     :post="post"
@@ -183,7 +181,6 @@ import GenderFilter from "@/components/common/GenderFilter";
 import MediaMedium from "@/components/common/profile/media/views/MediaMedium";
 import MediaSmall from "@/components/common/profile/media/views/MediaSmall";
 import BrowserStore from "store";
-// import mockStories from "@/mock/stories";
 
 export default {
   name: "Explore",
@@ -224,7 +221,6 @@ export default {
       return this.$store.state.explore.posts;
     },
     stories() {
-      // return mockStories;
       return this.$store.state.stories.explore.posts;
     },
     lives() {
