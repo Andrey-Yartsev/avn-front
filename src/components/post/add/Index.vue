998687532d68e56b6898f11e3783fa92a346e18c
ar.bylym <ar.bylym07@gmail.com>
2020-06-03 19:18:26 +0300

    Add postTipsGoal mobile view

--- src/components/post/add/Index.vue
+++ src/components/post/add/Index.vue
@@ -213,6 +213,10 @@
                 </span>
               </label>
             </div>
+            <span
+              @click="() => (tipsGoal.isEnabled = false)"
+              class="icn-item btn-reset btn-reset_prim-color icn-pos_center close-tipsGoal"
+            />
             <div class="form-group form-group_with-label amount">
               <label class="form-group-inner">
                 <span class="label">Amount:</span>
@@ -262,10 +266,10 @@
                 </span>
               </div>
             </div>
-            <span
+            <!-- <span
               @click="() => (tipsGoal.isEnabled = false)"
               class="icn-item btn-reset btn-reset_prim-color icn-pos_center close-tipsGoal"
-            />
+            /> -->
           </div>
         </div>
         <div class="actions-controls">
@@ -498,17 +502,81 @@
             </label>
           </div>
         </div>
-        <div
-          class="post-scheduled-time"
-          v-if="tipsGoal.isEnabled && $mq === 'mobile'"
-        >
-          <div class="datetime-value">
-            show
-            <!-- <span class="post-datetime__value">{{ formattedDateExpired }}</span>
+      </div>
+      <div
+        class="post-attachment"
+        v-if="tipsGoal.isEnabled && $mq === 'mobile'"
+      >
+        <div class="post-tipsGoal mobile-view">
+          <div class="post-tipsGoal-form">
             <span
-              @click="resetDatetimeExpired"
-              class="datetime-value__reset icn-item btn-reset btn-reset_prim-color icn-pos_center"
-            /> -->
+              @click="() => (tipsGoal.isEnabled = false)"
+              class="icn-item btn-reset btn-reset_prim-color icn-pos_center close-tipsGoal"
+            />
+            <div class="form-group form-group_with-label text">
+              <label class="form-group-inner">
+                <span class="label">Text:</span>
+                <span class="form-group form-group_clear-gaps">
+                  <span class="form-field">
+                    <input
+                      class="field-gap_currency"
+                      type="text"
+                      name="text"
+                      v-model="tipsGoal.text"
+                    />
+                  </span>
+                </span>
+              </label>
+            </div>
+            <div class="form-group form-group_with-label amount">
+              <label class="form-group-inner">
+                <span class="label">Amount:</span>
+                <span
+                  class="form-group form-group_clear-gaps field-symbol-currency"
+                >
+                  <span class="form-field">
+                    <input
+                      class="field-gap_currency"
+                      type="number"
+                      name="amount"
+                      v-model="tipsGoal.total"
+                    />
+                  </span>
+                </span>
+              </label>
+            </div>
+            <div class="form-group form-group_with-label sources">
+              <div class="form-group-inner">
+                <span class="label" :class="{ mobile: $mq === 'mobile' }"
+                  >Sources:</span
+                >
+                <span class="form-group form-group_clear-gaps">
+                  <span class="form-field">
+                    <multiselect
+                      v-model="tipsGoal.sources"
+                      :options="tipsGoalSourceTypes"
+                      :multiple="true"
+                      :close-on-select="true"
+                      :clear-on-select="false"
+                      :preserve-search="true"
+                      placeholder="Add source"
+                      label="title"
+                      track-by="value"
+                    >
+                      <template
+                        slot="selection"
+                        slot-scope="{ values, search, isOpen }"
+                        ><span
+                          class="multiselect__single"
+                          v-if="tipsGoal.sources.length &amp;&amp; !isOpen"
+                          >{{ tipsGoal.sources.length }} sources selected</span
+                        ></template
+                      >
+                    </multiselect>
+                  </span>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
