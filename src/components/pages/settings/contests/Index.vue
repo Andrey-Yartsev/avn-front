0a8490279bd08863aaf03dbb9a060ea32f805220
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2020-06-18 16:07:29 +0300

    Contest settings

---
+++ src/components/pages/settings/contests/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout ref="layout">
+    <Content slot="content" />
+    <template slot="title">
+      Contests
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "../Layout";
+import Content from "./Content";
+// import User from "@/mixins/user";
+
+export default {
+  name: "ContestsSettings",
+  // mixins: [User],
+  components: {
+    Layout,
+    Content
+  }
+};
+</script>
