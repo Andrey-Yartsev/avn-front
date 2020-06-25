077cbee220a2f89a797410dbd1e31343201a64fc
ar.bylym <ar.bylym07@gmail.com>
2020-06-25 18:42:37 +0300

    Update post tips select option to "All other tips"

--- src/components/post/add/Index.vue
+++ src/components/post/add/Index.vue
@@ -478,7 +478,7 @@ Settings.defaultLocale = "en";
 
 const tipsGoalSourceTypes = [
   { title: "Post tips", value: "localTips" },
-  { title: "All tips", value: "globalTips" },
+  { title: "All other tips", value: "globalTips" },
   { title: "Clips", value: "clips" },
   { title: "Messages", value: "messages" },
   { title: "Subscriptions", value: "subscriptions" }
