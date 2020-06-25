9c8054630a5f3195020081f86fdd106c22e97d78
ar.bylym <ar.bylym07@gmail.com>
2020-06-25 15:16:37 +0300

    Show spinner while fetching contests settings

--- src/components/pages/settings/contests/Content.vue
+++ src/components/pages/settings/contests/Content.vue
@@ -7,31 +7,50 @@
       Contests
     </h1>
     <div class="container shadow-block border-top cards">
-      <table class="info">
-        <tr>
-          <th>Contest</th>
-          <th>Rank</th>
-          <th>Date Range</th>
-          <th class="toggler"></th>
-        </tr>
-        <template v-if="items && items.length">
-          <Contest :data="v" v-for="v in items" :key="v.id" />
-        </template>
-        <tr v-else>
-          <td colspan="4">Empty here for now</td>
-        </tr>
-      </table>
+      <div class="loader-container" v-if="loading">
+        <Loader :fullscreen="false" text="" :small="true" />
+      </div>
+      <template v-else>
+        <div class="form-title" v-if="items && !items.length">
+          <div class="inner">
+            <div class="semi-transparent">
+              There is no available contests for now
+            </div>
+          </div>
+        </div>
+        <table v-else class="info">
+          <tr>
+            <th>Contest</th>
+            <th>Rank</th>
+            <th>Date Range</th>
+            <th class="toggler"></th>
+          </tr>
+          <template v-if="items && items.length">
+            <Contest :data="v" v-for="v in items" :key="v.id" />
+          </template>
+          <tr v-else>
+            <td colspan="4">Empty here for now</td>
+          </tr>
+        </table>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
 import Contest from "./Contest";
+import Loader from "@/components/common/Loader";
 
 export default {
   name: "ContestsSettingsContent",
   components: {
-    Contest
+    Contest,
+    Loader
+  },
+  data() {
+    return {
+      loading: true
+    };
   },
   computed: {
     items() {
@@ -39,7 +58,13 @@ export default {
     }
   },
   mounted() {
-    this.$store.dispatch("contest/settings/fetch");
+    this.$store
+      .dispatch("contest/settings/fetch")
+      .then()
+      .catch()
+      .finally(() => {
+        this.loading = false;
+      });
   }
 };
 </script>
