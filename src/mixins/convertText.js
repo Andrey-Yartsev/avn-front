f5fd41e321aec18f91e2b33cf948cf910faea09c
ar.bylym <ar.bylym07@gmail.com>
2020-12-22 17:06:16 +0200

    fix(posts) - Replace name to username in @user mentions in posts and clips

--- src/mixins/convertText.js
+++ src/mixins/convertText.js
@@ -10,7 +10,30 @@ export default {
         }
       );
       convertedText = convertedText.replace(/<br \/>/g, "\n");
+      convertedText = this.replaceTagToUsername(convertedText);
       return convertedText.replace(/(<([^>]+)>)/gi, "");
+    },
+    // replace user mentions - from <a data-login="user888" href="#">@some_namesdf</a> to <a data-login="user888" href="#">@user888</a>
+    replaceTagToUsername(string) {
+      if (!string || !string.length) {
+        return "";
+      }
+      let convertedString = string;
+      const keys = {};
+      const tags = string.split("</a>");
+      tags.forEach(item => {
+        const username = item.match(/"{1}[^"]+"{1}/i);
+        const name = item.match(/>@.+/i);
+        if (username && name) {
+          const key = name["0"].slice(1);
+          const value = username["0"].slice(1, -1);
+          keys[key] = value;
+        }
+      });
+      for (let key in keys) {
+        convertedString = convertedString.replace(key, `@${keys[key]}`);
+      }
+      return convertedString;
     }
   }
 };
